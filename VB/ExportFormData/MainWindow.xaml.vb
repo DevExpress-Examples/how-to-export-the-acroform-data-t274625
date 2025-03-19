ee973a9f8fbc60a04445a745cc214fb5b9be5392
DevExpressExampleBot <fakeEmail>
2025-03-19 14:55:29 +0400

    Source auto update [skip ci]

--- VB/ExportFormData/MainWindow.xaml.vb
+++ VB/ExportFormData/MainWindow.xaml.vb
@@ -1,17 +1,17 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace ExportFormData
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            Viewer.OpenDocument("..\..\FormFillDemo.pdf")
+            Me.InitializeComponent()
+            Me.Viewer.OpenDocument("..\..\FormFillDemo.pdf")
         End Sub
 
         Private Sub Viewer_DocumentLoaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Viewer.ExportFormData()
+            Me.Viewer.ExportFormData()
         End Sub
     End Class
 End Namespace
